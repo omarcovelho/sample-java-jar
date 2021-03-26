0306646fecdc83550094ceb467e66dbb093a81e4
Marco Prado <marco.prado@gsw.com.br>
2021-03-26 16:12:06 -0300

    Add tests

---
+++ src/test/java/br/com/omarcovelho/sample/DrinkTest.java
@@ -0,0 +1,17 @@
+package br.com.omarcovelho.sample;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * @author marprado - Marco Prado
+ * @version 1.0 26/03/2021
+ */
+class DrinkTest {
+
+    @Test
+    public void testDrink() {
+        assertTrue(true);
+    }
+}
